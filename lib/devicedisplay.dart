0578163ba5a291ad5921f1d19362fe95be95684f
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-20 18:51:38 -0300

    Make widget for device display in a separated module

---
+++ lib/devicedisplay.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class DeviceDisplay extends StatefulWidget {
+	final String devtag;
+	DeviceDisplay(@required this.devtag);
+	@override
+	_DeviceDisplayState createState() => _DeviceDisplayState(this.devtag);
+}
+	
+class _DeviceDisplayState extends State<DeviceDisplay> {
+	final String devtag;
+	_DeviceDisplayState(@required this.devtag);
+	@override
+	Widget build(BuildContext context) {
+		return Center(
+			child: Column(
+				mainAxisAlignment: MainAxisAlignment.center,
+				children: <Widget>[
+					Text(devtag),
+					Row (
+						mainAxisAlignment: MainAxisAlignment.center,
+						children: <Widget>[
+							IconButton(
+								icon: Icon(
+									Icons.lock,
+									semanticLabel: 'Lock',
+									size: 30,
+								),
+								onPressed: () {},
+							),
+							IconButton(
+								icon: Icon(
+									Icons.lightbulb,
+									semanticLabel: 'Lock',
+									size: 30,
+								),
+								onPressed: () {},
+							),
+							IconButton(
+								icon: Icon(
+									Icons.ac_unit,
+									semanticLabel: 'Lock',
+									size: 30,
+								),
+								onPressed: () {},
+								tooltip: 'Turn ON/OFF switch',
+							),
+						], //   row's children
+					), //   row 
+				], //   columns' children
+			), //   column
+		); //	Center
+	} //	Widget
+} //    class
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'devicedisplay.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,52 +29,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class DeviceDisplay extends Center {
-    final String tagname;
-    DeviceDisplay(this.tagname);
-    @override
-    var child = Column(
-      // Invoke "debug painting" (press "p" in the console, choose the
-      // "Toggle Debug Paint" action from the Flutter Inspector in Android
-      // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-      // to see the wireframe for each widget.
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-            const Text("oh god"),
-            Row (
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                    IconButton(
-                        icon: Icon(
-                            Icons.lock,
-                            semanticLabel: 'Lock',
-                            size: 30,
-                        ),
-                        onPressed: () {},
-                    ),
-                    IconButton(
-                        icon: Icon(
-                            Icons.lightbulb,
-                            semanticLabel: 'Lock',
-                            size: 30,
-                        ),
-                        onPressed: () {},
-                    ),
-                    IconButton(
-                        icon: Icon(
-                            Icons.ac_unit,
-                            semanticLabel: 'Lock',
-                            size: 30,
-                        ),
-                        onPressed: () {},
-                        tooltip: 'Turn ON/OFF switch',
-                    ),
-                ], //   row's children
-            ), //   row 
-        ], //   columns' children
-    ); //   column
-} //    class
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -89,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: DeviceDisplay('MyRoom'),
+      body: DeviceDisplay("Device Tag"),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
