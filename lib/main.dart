972f76f5f6a131ae7924bbe2102792968a4f71ef
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-20 16:00:05 -0300

    making a widget for device display

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,58 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class DeviceController extends Center {
+    @override
+    var child = Column(
+      // Invoke "debug painting" (press "p" in the console, choose the
+      // "Toggle Debug Paint" action from the Flutter Inspector in Android
+      // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+      // to see the wireframe for each widget.
+      //
+      // Column has various properties to control how it sizes itself and
+      // how it positions its children. Here we use mainAxisAlignment to
+      // center the children vertically; the main axis here is the vertical
+      // axis because Columns are vertical (the cross axis would be
+      // horizontal).
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+            const Text(
+                'TAG Device/Room',
+            ),
+            Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                    IconButton(
+                        icon: Icon(
+                            Icons.lock,
+                            semanticLabel: 'Lock',
+                            size: 30,
+                        ),
+                        onPressed: () {},
+                    ),
+                    IconButton(
+                        icon: Icon(
+                            Icons.lightbulb,
+                            semanticLabel: 'Lock',
+                            size: 30,
+                        ),
+                        onPressed: () {},
+                    ),
+                    IconButton(
+                        icon: Icon(
+                            Icons.ac_unit,
+                            semanticLabel: 'Lock',
+                            size: 30,
+                        ),
+                        onPressed: () {},
+                        tooltip: 'Turn ON/OFF switch',
+                    ),
+                ], //   row's children
+            ), //   row 
+        ], //   columns' children
+    ); //   column
+} //    class
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -43,56 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'TAG Device/Room',
-            ),
-            Row (
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                    IconButton(
-                            icon: Icon(
-                                Icons.lock,
-                                semanticLabel: 'Lock',
-                                size: 30,
-                            ),
-                            onPressed: () {},
-                    ),
-                    IconButton(
-                            icon: Icon(
-                                Icons.lightbulb,
-                                semanticLabel: 'Lock',
-                                size: 30,
-                            ),
-                            onPressed: () {},
-                    ),
-                    IconButton(
-                            icon: Icon(
-                                Icons.ac_unit,
-                                semanticLabel: 'Lock',
-                                size: 30,
-                            ),
-                            onPressed: () {},
-                            tooltip: 'Turn ON/OFF switch',
-                    ),
-                ],
-            ),
-          ],
-        ),
-      ),
+      body: DeviceController(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
