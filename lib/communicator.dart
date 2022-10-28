d6c86b1be86a0f85cdbb9348d45e11cba2643616
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-28 02:11:02 -0300

    using communicator as class

--- lib/communicator.dart
+++ lib/communicator.dart
@@ -1,33 +1,30 @@
 import 'dart:typed_data';
 import 'dart:io';
 
-void main() {
-	talk_to_server();
-}
+class Net {
+	static void talk_to_server() async {
+		final socket = await Socket.connect('localhost', 2000);
+		print('Connected to ${socket.remoteAddress.address}:${socket.remotePort}');
+		await Net.sendCommands(socket, '{ "type":"client" }\n');
+		socket.listen(
+			(Uint8List data) {
+				final serverResponse = String.fromCharCodes(data);
+				print('Server: $serverResponse');
+			},
 
-void talk_to_server() async {
-	final socket = await Socket.connect('localhost', 2000);
-	print('Connected to ${socket.remoteAddress.address}:${socket.remotePort}');
-	await sendCommands(socket, '{ "type":"client" }\n');
-	socket.listen(
-		(Uint8List data) {
-			final serverResponse = String.fromCharCodes(data);
-			print('Server: $serverResponse');
-		},
+			onError:(error) {
+				print(error);
+				socket.destroy();
+			},
 
-		onError:(error) {
-			print(error);
-			socket.destroy();
-		},
-
-		onDone: () {
-			print('Server left.');
-			socket.destroy();
-		},
-	);
-}
-
-Future<void> sendCommands(Socket socket, String message) async {
-	print('Client: $message');
-	socket.write(message);
+			onDone: () {
+				print('Server left.');
+				socket.destroy();
+			},
+		);
+	}
+	static Future<void> sendCommands(Socket socket, String message) async {
+		print('Client: $message');
+		socket.write(message);
+	}
 }
