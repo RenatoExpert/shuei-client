65912c46361914b3d7e58cb1ea752231ce62501b
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-29 16:21:14 -0300

    rename everything on project from janus supervisor to shuei client

--- android/app/src/main/kotlin/com/example/janus_supervisor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.janus_supervisor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:janus_supervisor/main.dart';
+import 'package:shuei_client/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
