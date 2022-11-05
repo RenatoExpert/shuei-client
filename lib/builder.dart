337f120c3869a17df9cfbc080f4558d1b306e465
Renato Araujo <renatorraraujo@gmail.com>
2022-11-05 20:54:57 +0000

    16 Show connection states (#23)
    * making different return for different stream states
    
    * say when connection is lost, in progress or done
    
    * replacing function wait_disconnect for builder ConnectionState.done

--- lib/builder.dart
+++ lib/builder.dart
@@ -12,22 +12,14 @@ final port = 2000;
 var main_socket;
 var main_stream;
 
-wait_disconnect () async {
-	main_stream.drain().then((_) {
-		main_socket.close();
-		print("Socket disconnected");
-		sleep(Duration(seconds:1));
-		connect();
-	});
-}
 connect () async {
 	print('Connecting...');
 	while (true) {
 		try {
 			main_socket = await Socket.connect(host, port);
+			print('Connected!');
 			main_stream = main_socket.asBroadcastStream();
 			await main_socket.write('{"type":"client"}\n');
-			wait_disconnect();
 			break;
 		} catch(e) {
 			print("Server connection failed ${e}");
@@ -43,28 +35,47 @@ var builder = StreamBuilder<dynamic>(
 		BuildContext context,
 		AsyncSnapshot<dynamic> snapshot,
 	) {
-		try {
-			try {
-				current_states = jsonDecode(
-					String.fromCharCodes(snapshot.data)
-				);
-			} catch (e) {
-				print(e);
-			}
-			if (current_states.isEmpty) {
-				return Text('Listening for new gadgets...');
-			} else if (current_states.length > 0) {
-				return Column (
-					children: List.generate(current_states.length, (index) {
-							return DeviceDisplay(current_states.keys.elementAt(index), main_socket);
-					}),
-					mainAxisAlignment: MainAxisAlignment.center,
-				);
-			} else {
-				return Text("Unknown error");
-			}
-		} catch (e) {
-			return Text("Error: $e");
+		switch (snapshot.connectionState) {
+			case ConnectionState.waiting:
+				return Text('Connected! Fetching data...');
+			case ConnectionState.none:
+				return Text('Things are so calm by here...');
+			case ConnectionState.done:
+				main_socket.close();
+				print("Socket disconnected");
+				sleep(Duration(seconds:1));
+				connect();
+				return Text('Connection is lost.\nTrying to reconnect...');
+			case ConnectionState.active:
+				if (snapshot.hasData) {
+					try {
+						try {
+							final raw_string = String.fromCharCodes(snapshot.data);
+							print(raw_string);
+							current_states = jsonDecode(raw_string);
+						} catch (e) {
+							print("Parsing info ${e}");
+						}
+						if (current_states.isEmpty) {
+							return Text('Listening for new gadgets...');
+						} else if (current_states.length > 0) {
+							return Column (
+								children: List.generate(current_states.length, (index) {
+										return DeviceDisplay(current_states.keys.elementAt(index), main_socket);
+								}),
+								mainAxisAlignment: MainAxisAlignment.center,
+							);
+						} else {
+							return Text("Unknown error");
+						}
+					} catch (e) {
+						return Text("Error: $e");
+					}
+				} else if (snapshot.hasError) {
+					return Text("${snapshot.error}");
+				} else {
+					return CircularProgressIndicator();
+				}
 		}
 	}
 );
