a00e1f4252f9953ac31d15274f9ec16739b2a8a3
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-11-06 00:08:43 -0300

    Turn most widgets into stateless

--- lib/devicedisplay.dart
+++ lib/devicedisplay.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'gadgetbutton.dart';
 
-class DeviceDisplay extends StatefulWidget {
+class DeviceDisplay extends StatelessWidget {
 	final String uuid;
 	final Socket sender;
 	DeviceDisplay(@required this.uuid, @required this.sender);
-	@override
-	_DeviceDisplayState createState() => _DeviceDisplayState(this.uuid, this.sender);
-}
-
-class _DeviceDisplayState extends State<DeviceDisplay> {
-	final String uuid;
-	final Socket sender;
-	_DeviceDisplayState(@required this.uuid, @required this.sender);
 	@override
 	Widget build(BuildContext context) {
 		return Center(
@@ -23,9 +16,9 @@ class _DeviceDisplayState extends State<DeviceDisplay> {
 					Row (
 						mainAxisAlignment: MainAxisAlignment.center,
 						children: <Widget>[
-							GenericIconButton (Icons.lock, 'Lock', 0, this.uuid, this.sender),
-							GenericIconButton (Icons.lightbulb, 'Light', 1, this.uuid, this.sender),
-							GenericIconButton (Icons.ac_unit, 'Air conditioner', 2, this.uuid, this.sender),
+							GadgetButton (Icons.lock, 'Lock', 0, this.uuid, this.sender),
+							GadgetButton (Icons.lightbulb, 'Light', 1, this.uuid, this.sender),
+							GadgetButton (Icons.ac_unit, 'Air conditioner', 2, this.uuid, this.sender),
 						], //   row's children
 					), //   row 
 				], //   columns' children
@@ -33,71 +26,4 @@ class _DeviceDisplayState extends State<DeviceDisplay> {
 		); //	Center
 	} //	Widget
 } //    class
-
-class GenericIconButton extends StatefulWidget {
-	final IconData iconsym;
-	final String radical;
-	final int gadget_index;
-	final String uuid;
-	final Socket sender;
-	GenericIconButton(@required this.iconsym, @required this.radical, @required this.gadget_index, @required this.uuid, @required this.sender);
-	@override
-	_GenericIconButtonState createState() => _GenericIconButtonState(this.iconsym, this.radical, this.gadget_index, this.uuid, this.sender);
-}
-
-class _GenericIconButtonState extends State<GenericIconButton> {
-	final IconData iconsym;
-	final String radical;
-	final int gadget_index;
-	final String uuid;
-	final Socket sender;
-	String lock_state () {
-		return '2';
-		/*
-		if (current_states.containsKey(this.uuid)) {
-			return current_states[this.uuid].toString()[gadget_index];
-		} else {
-			return 'null';
-		}
-		*/
-	}
-	get tooltip_render { 
-		var isActive = lock_state() == '2'|| lock_state() =='3'; 
-		var execution = isActive  ? 'Allowed' : 'Disabled';
-		var inverse = isActive ? 'disable' : 'enable';
-		return "${radical} is ${execution}. Click to ${inverse}";
-	}
-	get iconColor {
-		switch (lock_state()) {
-			case '0':
-				return Colors.black;
-				break;
-			case '1':
-				return Colors.red;
-				break;
-			case '2':
-				return Colors.yellow;
-				break;
-			case '3':
-				return Colors.green;
-				break;
-		}
-	}
-	_GenericIconButtonState(@required this.iconsym, @required this.radical, @required this.gadget_index, @required this.uuid, @required this.sender);
-	@override
-	Widget build(BuildContext context) {
-		return IconButton(
-			icon: Icon(
-				iconsym,
-				color: iconColor,
-				semanticLabel: radical,
-				size: 30,
-			),
-			onPressed: () {
-				this.sender.write('{ "uuid": "mycontroller", "command": "setstate", "args":{ "gpio":"3", "pinstate":"1"} }\n');
-			},
-			tooltip: tooltip_render,
-		);
-	}	
-}
 
---
+++ lib/gadgetbutton.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class GadgetButton extends StatelessWidget {
+	final IconData iconsym;
+	final String radical;
+	final int gadget_index;
+	final String uuid;
+	final Socket sender;
+	String lock_state () {
+		return '2';
+	}
+	get tooltip_render { 
+		var isActive = lock_state() == '2'|| lock_state() =='3'; 
+		var execution = isActive  ? 'Allowed' : 'Disabled';
+		var inverse = isActive ? 'disable' : 'enable';
+		return "${radical} is ${execution}. Click to ${inverse}";
+	}
+	get iconColor {
+		switch (lock_state()) {
+			case '0':
+				return Colors.black;
+				break;
+			case '1':
+				return Colors.red;
+				break;
+			case '2':
+				return Colors.yellow;
+				break;
+			case '3':
+				return Colors.green;
+				break;
+		}
+	}
+	GadgetButton(@required this.iconsym, @required this.radical, @required this.gadget_index, @required this.uuid, @required this.sender);
+	@override
+	Widget build(BuildContext context) {
+		return IconButton(
+			icon: Icon(
+				iconsym,
+				color: iconColor,
+				semanticLabel: radical,
+				size: 30,
+			),
+			onPressed: () {
+				this.sender.write('{ "uuid": "mycontroller", "command": "setstate", "args":{ "gpio":"3", "pinstate":"1"} }\n');
+			},
+			tooltip: tooltip_render,
+		);
+	}	
+}
+
